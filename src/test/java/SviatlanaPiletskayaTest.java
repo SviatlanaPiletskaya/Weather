5189fd0983b110012a00d954bafdde87f60f6cb7
= <=>
2022-11-07 16:00:10 +0300

    added test from TC11-01 to TC11-05

--- src/test/java/SviatlanaPiletskayaTest.java
+++ src/test/java/SviatlanaPiletskayaTest.java
@@ -6,6 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 public class SviatlanaPiletskayaTest {
 
     //TC_1_1  - Тест кейс:
@@ -67,46 +69,29 @@ public class SviatlanaPiletskayaTest {
 2.  Нажать на пункт меню Guide
 3.  Подтвердить, что вы перешли на страницу со ссылкой https://openweathermap.org/guide
 и что title этой страницы OpenWeatherMap API guide - OpenWeatherMap*/
-
-@Ignore
     @Test
-    public void test() throws InterruptedException {
+    public void testGuideUrlAndHeader() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/svetikpileckaa/Applications/ChromeDriver/chromedriver");
         WebDriver driver = new ChromeDriver();
 
         String url = "https://openweathermap.org/";
-
-        String expectedResult = "OpenWeatherMap API guide - OpenWeatherMap";
+        String expectedResultTitle = "OpenWeatherMap API guide - OpenWeatherMap";
+        String expectedResultUrl = "https://openweathermap.org/guide";
 
         driver.get(url);
         Thread.sleep(5000);
 
-        WebElement searchButton = driver.findElement(
-                By.xpath("//*[@id=\"desktop-menu\"]/ul/li[1]/a")
+        WebElement guideElementMenu = driver.findElement(
+                By.xpath("//a[@href='/guide']")
         );
-        searchButton.click();
+        guideElementMenu.click();
+        String actualResultUrl = driver.getCurrentUrl();
+        String actualResultTitle = driver.getTitle();
 
-
-        Thread.sleep(1000);
-
-        WebElement parisFRChoiceInDropdownMenu = driver.findElement(
-                By.xpath("//ul[@class='search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
-        );
-        parisFRChoiceInDropdownMenu.click();
-
-        WebElement h2CityCountryHeader = driver.findElement(
-                By.xpath("//div[@id = 'weather-widget']//h2")
-        );
-
-        Thread.sleep(2000);
-        String actualResult = h2CityCountryHeader.getText();
-
-        Assert.assertEquals(actualResult, expectedResult);
-
-//        Thread.sleep(5000);
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+        Assert.assertEquals(actualResultTitle, expectedResultTitle);
 
         driver.quit();
-//        driver.close();
     }
 
 /*    TC_11_02
@@ -125,15 +110,15 @@ public class SviatlanaPiletskayaTest {
         driver.get(url);
         Thread.sleep(5000);
 
-        WebElement changeImperialByF = driver.findElement(
+        WebElement menuImperialByF = driver.findElement(
                 By.xpath("//*[@id=\"weather-widget\"]/div[1]/div/div/div[1]/div[2]/div[3]")
         );
 
-        changeImperialByF.click();
+        menuImperialByF.click();
         Thread.sleep(5000);
 
         WebElement getImperialName = driver.findElement(
-                By.xpath("//div[@id = 'weather-widget']//div[@class = 'section-content']//span[@class = 'heading']")
+                By.xpath("//span[@class = 'heading']")
         );
 
         String newImperial = getImperialName.getText();
@@ -146,43 +131,124 @@ public class SviatlanaPiletskayaTest {
     }
 
 
-
     /*TC_11_03
 1.  Открыть базовую ссылку
 2. Подтвердить, что внизу страницы есть панель с текстом “We use cookies which are essential for the site to work. We also use non-essential cookies to help us improve our services. Any data collected is anonymised. You can allow all cookies or manage them individually.”
 3. Подтвердить, что на панели внизу страницы есть 2 кнопки “Allow all” и “Manage cookies”*/
-
-    public void checkingForCookies() throws InterruptedException {
+    @Test
+    public void testCheckingForCookies() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/svetikpileckaa/Applications/ChromeDriver/chromedriver");
         WebDriver driver = new ChromeDriver();
-        String actualResult = "We use cookies which are essential for the site to work. "
+        String expectedResult = "We use cookies which are essential for the site to work. "
                 + "We also use non-essential cookies to help us improve our services. Any data collected is anonymised. "
-                + " You can allow all cookies or manage them individually.";
+                + "You can allow all cookies or manage them individually.";
+        String button1Text = "Allow all";
+        String button2Text = "Manage cookies";
+
         String url = "https://openweathermap.org/";
         driver.get(url);
         Thread.sleep(5000);
 
+        WebElement elementWithCookiesOnPage = driver.findElement(By.xpath("//div[@class = 'stick-footer-panel__container']/p"));
+        WebElement buttonAllowAll = driver.findElement(By.xpath("//button[text()= 'Allow all']"));
+        WebElement buttonManageCookies = driver.findElement(By.xpath("//a[@href='/cookies-settings']"));
 
+        String actualResult = elementWithCookiesOnPage.getText();
 
+        Assert.assertEquals(actualResult, expectedResult);
+
+        Assert.assertEquals(driver.findElements(By.xpath("//div[@class='stick-footer-panel__btn-container']/*")).size(), 2);
+
+//        Assert.assertTrue(driver.findElement(
+//                        By.xpath("//div[@class = 'stick-footer-panel__container']/*[text() = '" + button1Text + "]")).isDisplayed());
+//
+//        Assert.assertTrue(driver.findElement(
+//                        By.xpath("//div[@class = 'stick-footer-panel__container']/*[text() = '" + button2Text + "]")).isDisplayed());
+
+        Assert.assertEquals(buttonAllowAll.getText(), "Allow all");
+        Assert.assertEquals(buttonManageCookies.getText(), "Manage cookies");
+
+        driver.quit();
     }
 
+    /*TC_11_04
+    1.  Открыть базовую ссылку
+    2.  Подтвердить, что в меню Support есть 3 подменю с названиями “FAQ”, “How to start” и “Ask a question”*/
+    @Test
+    public void testSupportMenu() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/svetikpileckaa/Applications/ChromeDriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        String expectedResultFAQ = "FAQ";
+        String expectedResultHowToStart = "How to start";
+        String expectedResultAskAQuestion = "Ask a question";
 
+        String url = "https://openweathermap.org/";
+        driver.get(url);
+        Thread.sleep(5000);
 
+        WebElement elementSupport = driver.findElement(By.xpath("//div[@id='support-dropdown']"));
+        elementSupport.click();
 
+        WebElement subMenuFaq = driver.findElement(By.xpath("//a[@href='/faq']"));
+        WebElement subMenuHowToStart = driver.findElement(By.xpath("//a[@href='/appid']"));
+        WebElement subMenuAskAQuestion = driver.findElement(By.xpath("//a[@href='https://home.openweathermap.org/questions']"));
 
+        Assert.assertEquals(driver.findElements(By.xpath("//ul[@class='dropdown-menu dropdown-visible']/li")).size(), 3);
+        Assert.assertEquals(subMenuFaq.getText(), expectedResultFAQ);
+        Assert.assertEquals(subMenuHowToStart.getText(), expectedResultHowToStart);
+        Assert.assertEquals(subMenuAskAQuestion.getText(), expectedResultAskAQuestion);
+        driver.quit();
+    }
 
+    /*TC_11_05
+1. Открыть базовую ссылку
+2. Нажать пункт меню Support → Ask a question
+3. Заполнить поля Email, Subject, Message
+4. Не подтвердив CAPTCHA, нажать кнопку Submit
+5. Подтвердить, что пользователю будет показана ошибка “reCAPTCHA verification failed, please try again.”*/
 
+    @Test
+    public void testCaptchaErrorMessage() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/svetikpileckaa/Applications/ChromeDriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        String email = "test@test.com";
+        String message = "Test";
+        String expectedResultTextCAPTHA = "reCAPTCHA verification failed, please try again.";
 
+        String url = "https://openweathermap.org/";
+        driver.get(url);
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
 
+        WebElement elementSupport = driver.findElement(By.xpath("//div[@id='support-dropdown']"));
+        elementSupport.click();
 
-//    @Test
-//    public void test_name() {
-//        System.setProperty("webdriver.chrome.driver", "/Users/svetikpileckaa/Applications/ChromeDriver/chromedriver");
-//        WebDriver driver = new ChromeDriver();
-//
-//
-//
-//        driver.quit();
-//        driver.close();
-//    }
+        WebElement subMenuAskAQuestion = driver.findElement(By.xpath("//a[@href='https://home.openweathermap.org/questions']"));
+        subMenuAskAQuestion.click();
+
+        ArrayList<String> page = new ArrayList<String>(driver.getWindowHandles()); //переходит на новую страницу
+        driver.switchTo().window(page.get(1));
+
+        WebElement inputEmail = driver.findElement(By.xpath("//input[@class = 'form-control string email required']"));
+        inputEmail.click();
+        inputEmail.sendKeys(email);
+
+        WebElement selectField = driver.findElement(By.xpath("//select[@class='form-control select required']"));
+        selectField.click();
+
+        WebElement selectFieldChoice = driver.findElement(By.xpath("//option[1]"));
+        selectFieldChoice.click();
+
+        WebElement inputMessage = driver.findElement(By.xpath("//textarea[@class='form-control text required']"));
+        inputMessage.sendKeys(message);
+
+        WebElement submitButton = driver.findElement(By.xpath("//input[@type='submit'][@name='commit']"));
+        submitButton.click();
+        Thread.sleep(2000);
+
+        WebElement reCapthaText = driver.findElement(By.xpath("//div[@class='help-block']"));
+
+        Assert.assertEquals(reCapthaText.getText(), expectedResultTextCAPTHA);
+        driver.quit();
+    }
 }
